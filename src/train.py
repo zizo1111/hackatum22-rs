dd7df01928a9794c73f95b62ce4c68d1e6a5f0a2
Your Name <walter.zimmer@tum.de>
2022-11-19 11:43:21 +0100

    fix out of memory bug

--- src/train.py
+++ src/train.py
@@ -21,23 +21,26 @@ def train_one_epoch(training_loader, optimizer, model, loss_fn, epoch_index, tb_
         labels = data['labels'].to(device)
 
         # Zero your gradients for every batch!
-        optimizer.zero_grad()
+        #optimizer.zero_grad()
 
         # Make predictions for this batch
         outputs = model(inputs)
-    
+
         # Compute the loss and its gradients
-        loss = loss_fn(outputs, labels)
-        print(loss.values())
+        loss = loss_fn(outputs.float(), labels.float())
+
+        # print(loss.values())
         loss.backward()
 
         # Adjust learning weights
         optimizer.step()
 
+        optimizer.zero_grad()
+
         # Gather data and report
         running_loss += loss.item()
-        if i % 1000 == 999:
-            last_loss = running_loss / 1000 # loss per batch
+        if i % 100 == 99:
+            last_loss = running_loss / 100  # loss per batch
             print('  batch {} loss: {}'.format(i + 1, last_loss))
             tb_x = epoch_index * len(training_loader) + i + 1
             tb_writer.add_scalar('Loss/train', last_loss, tb_x)
@@ -46,11 +49,9 @@ def train_one_epoch(training_loader, optimizer, model, loss_fn, epoch_index, tb_
     return last_loss
 
 
-
-
-
 def train(device):
-    full_dataset = MicrowaveDataset('../data/dummy_measurements/volumes', '../data/dummy_measurements/labels_transformed.json')
+    full_dataset = MicrowaveDataset('../data/dummy_measurements/volumes',
+                                    '../data/dummy_measurements/labels_transformed.json')
     train_size = int(0.8 * len(full_dataset))
     test_size = len(full_dataset) - train_size
     train_dataset, val_dataset = torch.utils.data.random_split(full_dataset, [train_size, test_size])
@@ -60,12 +61,9 @@ def train(device):
 
     loss_fn = torch.nn.MSELoss()
 
-    
-
     model = resnet50(num_classes=6)
     # Optimizers specified in the torch.optim package
     optimizer = torch.optim.Adam(params=model.parameters(), lr=1e-4)
-
 
     # Initializing in a separate cell so we can easily add more epochs to the same run
     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
@@ -76,24 +74,29 @@ def train(device):
 
     best_vloss = 1_000_000.
 
+    model.to(device)
     for epoch in range(EPOCHS):
         print('EPOCH {}:'.format(epoch_number + 1))
 
         # Make sure gradient tracking is on, and do a pass over the data
         model.train(True)
-        model.to(device)
         avg_loss = train_one_epoch(training_loader, optimizer, model, loss_fn, epoch_number, writer, device)
 
         # We don't need gradients on to do reporting
         model.train(False)
 
+        model.eval()
+        torch.cuda.empty_cache()
+
+
         running_vloss = 0.0
-        for i, vdata in enumerate(validation_loader):
-            vinputs = vdata['inputs'].to(device)
-            vlabels = vdata['labels'].to(device)
-            voutputs = model(vinputs)
-            vloss = loss_fn(voutputs, vlabels)
-            running_vloss += vloss
+        with torch.no_grad():
+            for i, vdata in enumerate(validation_loader):
+                vinputs = vdata['inputs'].to(device)
+                vlabels = vdata['labels'].to(device)
+                voutputs = model(vinputs)
+                vloss = loss_fn(voutputs.float().detach(), vlabels.float().detach())
+                running_vloss += vloss
 
         avg_vloss = running_vloss / (i + 1)
         print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
@@ -101,8 +104,8 @@ def train(device):
         # Log the running loss averaged per batch
         # for both training and validation
         writer.add_scalars('Training vs. Validation Loss',
-                        { 'Training' : avg_loss, 'Validation' : avg_vloss },
-                        epoch_number + 1)
+                           {'Training': avg_loss, 'Validation': avg_vloss},
+                           epoch_number + 1)
         writer.flush()
 
         # Track best performance, and save the model's state
@@ -112,12 +115,14 @@ def train(device):
             torch.save(model.state_dict(), model_path)
 
         epoch_number += 1
+        torch.cuda.empty_cache()
+
 
 if __name__ == '__main__':
 
-
     device = torch.device("cpu")
     if torch.cuda.is_available():
-        device = torch.cuda.current_device()
-    
+        # device = torch.cuda.current_device()
+        device = 'cuda'
+
     train(device)
